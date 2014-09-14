54e6c0929c4a56d8999bc2b652dd98bfac5d84fb
K-2052 <k@2052.me>
2014-09-14 08:46:33 -0700

    add valid helper and taskResp helper to response helpers

--- app/helpers/resp.rb
+++ app/helpers/resp.rb
@@ -1,11 +1,27 @@
 module Afraid
   module Helpers
     module Resp
+      def valid?(object)
+        valid = true
+        # `valid?` method may override existing errors, so check for those first
+        valid &&= object.valid? if object.respond_to?(:valid?)
+        valid &&= (object.errors.count == 0) if object.respond_to?(:errors)
+        return valid
+      end
+
       def resp(obj)
-        if obj.valid?
+        if valid? obj
           obj
         else
           error!(obj.errors.to_json, 400)
+        end
+      end
+
+      def taskResp(task)
+        if task.valid?
+          TasksSerializer.new([task])  
+        else
+          error!({errors: task.errors.to_a}, 400)
         end
       end
     end
