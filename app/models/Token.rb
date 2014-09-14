39e157e1d45392b193cf72ba61363f73e7f10e6f
K-2052 <k@2052.me>
2014-09-14 08:41:39 -0700

    make token belong to user model

--- app/models/Token.rb
+++ app/models/Token.rb
@@ -8,7 +8,7 @@ class Token
 
   before_create :generate_access_token
   before_create :set_expiration
-  belongs_to :account
+  belongs_to :user
 
   def expired?
     DateTime.now >= self.expires_at
