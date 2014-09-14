09dbf4eae3b85edb6dca119efd7d2bad3ce96eee
K-2052 <k@2052.me>
2014-09-14 08:49:40 -0700

    add correct properties to task serializer

--- app/serializers/task.rb
+++ app/serializers/task.rb
@@ -5,7 +5,13 @@ class TaskSerializer < Oat::Serializer
 
   schema do
     type "tasks"
-    property :id, item.id
-    property :title, item.title
+		link :self, href: '/tasks'
+
+		property :href,       "https://api.afraidTO.do/tasks/#{item.id}"
+    property :id,         item.id
+    property :text,       item.text
+    property :done,       item.done
+    property :created_at, item.created_at
+    property :updated_at, item.updated_at
   end
 end
