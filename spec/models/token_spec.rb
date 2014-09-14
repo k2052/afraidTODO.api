de8811540d2924c0cd4d515b9de5c69fcbc1886d
K-2052 <k@2052.me>
2014-09-14 08:53:01 -0700

    fix up token spec

--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,22 +1,22 @@
 describe "Token" do
 	it "should create a token" do
 		token = FactoryGirl.create(:token)
-		token = ApiKey.find token.id
+		token = Token.find token.id
 		token.should be_an_instance_of Token
 	end
 
-	it "should create a token with an account" do
-		account = FactoryGirl.create(:account)
-		account.tokens.first().should be_an_instance_of Token
+	it "should create a token with an user" do
+		user = FactoryGirl.create(:user)
+		user.tokens.first().should be_an_instance_of Token
 	end
 
 	it "should destroy a token" do 
 		token = FactoryGirl.create(:token)
-		token = Token.find api_key.id
+		token = Token.find token.id
 		token.should be_an_instance_of Token
 		token_id = token.id
 		token.destroy()
-		Token.find(api_key_id).should be_nil
+		Token.find(token_id).should be_nil
 	end
 
 	it "should indicate when a token is expired" do
