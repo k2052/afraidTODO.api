f55efbd88122c3b227dea154506eaa7cb1517c8e
K-2052 <k@2052.me>
2014-09-20 09:02:40 -0700

    cleanup boot file

--- config/boot.rb
+++ config/boot.rb
@@ -2,5 +2,5 @@ ENV['RACK_ENV'] ||= 'development'
 
 require 'bundler/setup'
 
-Bundler.require :default, ENV['RACK_ENV'].to_sym || :development
+Bundler.require :default, ENV['RACK_ENV'].to_sym
 I18n.enforce_available_locales = false
