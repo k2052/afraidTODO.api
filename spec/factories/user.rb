f668515c7e1c89a8307e49306c839911fea1eb7e
K-2052 <k@2052.me>
2014-09-20 09:18:25 -0700

    update user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,8 +3,6 @@ FactoryGirl.define do
     email { "#{Faker::Internet.user_name}_#{rand(0...1000)}@example.org" }
     username { Faker::Internet.user_name.gsub('.','') + "_#{rand(0...20000)}" }
     password 'testpass'
-    password_confirmation 'testpass'
     name { Faker::Name.name }
-    role 'test'
   end
 end
